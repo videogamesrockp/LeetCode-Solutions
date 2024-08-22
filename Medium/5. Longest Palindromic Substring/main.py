b3b5b9450ca95f9a4c534daf3fbf551116731af3
Aster Deng <46637392+videogamesrockp@users.noreply.github.com>
2024-08-21 17:49:25 -0700

    Delete Medium directory

--- Medium/5. Longest Palindromic Substring/main.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        ans=s[0]
-        for i in range(len(s)):
-            for j in range(i, len(s) + 1):
-                text = s[i:j]
-                if text == text[::-1] and len(ans) < len(text):
-                    ans = text
-        return(ans)
