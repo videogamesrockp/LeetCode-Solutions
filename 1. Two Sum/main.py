68b53d572f7027217bf235927fd95a69e2b9368d
Aster Deng <46637392+videogamesrockp@users.noreply.github.com>
2024-08-21 17:58:08 -0700

    Create main.py

---
+++ 1. Two Sum/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i in range(len(nums)):
+            if (target - nums[i]) in dict:
+                return [i, dict[target - nums[i]]]
+            dict[nums[i]] = i
